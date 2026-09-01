ef8d62256982df0ad1e210d93209d9dad7d8dd5d
Hayden Swain-Schmit <swainschmity@gmail.com>
2026-09-01 12:12:34 -0600

    CharacterCard complete with props

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
   return (
     <div>
       <h1>Character Selection</h1>
-      <CharacterCard />
+      <CharacterCard name="Knight"
+       desc="Fierce warrior in hand-to-hand combat"
+       exp="1600"/>
+
+      <CharacterCard name="Valkyrie"
+       desc="Agile combatant with distance attacks"
+       exp="900"/>
     </div>
   );
 }
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,10 +1,10 @@
-function CharacterCard() {
+function CharacterCard(props) {
     return (
       <div>
-        <h2>Knight</h2>
-        <p>Description: Fierce warrior in hand-to-hand combat</p>
-        <p>Experience: 1600</p>
-        <p>Level: {1600 / 100}</p>
+        <h2>{props.name}</h2>
+        <p>Description: {props.desc}</p>
+        <p>Experience: {props.exp}</p>
+        <p>Level: {props.exp / 100}</p>
       </div>
     );
 }
